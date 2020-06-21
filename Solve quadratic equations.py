d6027a4b09721e4fdd34136e6e8b05405d092cbc
sakshamsethi123 <67079478+sakshamsethi123@users.noreply.github.com>
2020-06-21 18:44:02 -0400

    Update Solve quadratic equations.py

--- Solve quadratic equations.py
+++ Solve quadratic equations.py
@@ -12,6 +12,16 @@ if a==0:
   print ("What? That is not a quadratic equation! For it to be a quadratic, the variable A has to be greater than 0.")
 else: 
   blahblahblah=b**2 - 4*a*c
+
+if blahblahblah < 0:
+  blahblahblah = blahblahblah * -1
+  blahblahblahSQRT = math.sqrt(blahblahblah)
+  realPart = (0-b)/(2*a)
+  imaginaryPart = blahblahblahSQRT / (2*a)
+  print(f'({realPart:f} + {imaginaryPart:f}i)')
+  print(f'({realPart:f} - {imaginaryPart:f}i)')
+  
+else:  
   x1 = (0-b + math.sqrt(blahblahblah))/(2*a)
   x2 = (0-b - math.sqrt(blahblahblah))/(2*a)
   print ("The two solutions are...")
