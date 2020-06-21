bc6ecb05f60afa2aa5ee1037990a7b8a0346bfb4
sakshamsethi123 <67079478+sakshamsethi123@users.noreply.github.com>
2020-06-20 20:27:17 -0400

    Update How many days until.py

--- How many days until.py
+++ How many days until.py
@@ -12,72 +12,72 @@ year2=input("What year do you want to measure until? Answer in the form: 2020 \n
 year2=int(year2) 
 if year1 % 4 == 0:  
   if month1>month2 and date1>date2: 
-    print "That date is after..."
-    print  32 - date1 + 1 + date2 - 1
-    print "day(s)"
-    print 12 - month1 + month2 - 1
-    print "month(s)" 
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print ( 32 - date1 + 1 + date2 - 1)
+    print ("day(s)")
+    print (12 - month1 + month2 - 1)
+    print ("month(s)") 
+    print (year2 - year1)
+    print ("year(s)")
 
   if month1>month2 and date1==date2: 
-    print "That date is after..."
-    print "0"
-    print "day(s)"
-    print 12 - month1 + month2 - 1
-    print "month(s)" 
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print ("0")
+    print ("day(s)")
+    print (12 - month1 + month2 - 1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
     
   if month1>month2 and date2>date1: 
-    print "That date is after..."
-    print  32 - date1 + 1 + date2 - 1
-    print "day(s)"
-    print 12 - month1 + month2 - 1
-    print "month(s)" 
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print  (32 - date1 + 1 + date2 - 1)
+    print ("day(s)")
+    print (12 - month1 + month2 - 1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
 
 
 
 
   if month2>month1 and date1>date2: 
-    print "That date is after..."
-    print month2 - month1
-    print "month(s)"
-    print 32 - date1 + 1 + date2 - 1
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print (month2 - month1)
+    print ("month(s)")
+    print (32 - date1 + 1 + date2 - 1)
+    print (year2 - year1)
+    print ("year(s)")
     
   if month2>month1 and date1==date2: 
-    print "That date is after..."
-    print "0"
-    print "day(s)"
-    print month2 - month1
-    print "month(s)"
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print ("0")
+    print ("day(s)")
+    print (month2 - month1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
   
   if month2>month1 and date2>date1:  
-    print "That date is after..."
-    print date2 - date1
-    print "day(s)"
-    print month2 - month1
-    print "month(s)"
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print (date2 - date1)
+    print ("day(s)")
+    print (month2 - month1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
     
  
  
  
   if month2==month1 and date2==date1: 
-    print "That date is after..."
-    print date2 - date1
-    print "day(s)"
-    print month2 - month1
-    print "month(s)"
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print (date2 - date1)
+    print ("day(s)")
+    print (month2 - month1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
   
     
    
@@ -86,72 +86,57 @@ if year1 % 4 == 0:
 
 else: 
    if month1>month2 and date1>date2: 
-    print "That date is after..."
-    print  31 - date1 + 1 + date2 - 1
-    print "day(s)"
-    print 12 - month1 + month2 - 1
-    print "month(s)" 
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print  (31 - date1 + 1 + date2 - 1)
+    print ("day(s)")
+    print (12 - month1 + month2 - 1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
 
    if month1>month2 and date1==date2: 
-    print "That date is after..."
-    print "0"
-    print "day(s)"
-    print 12 - month1 + month2 - 1
-    print "month(s)" 
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print ("0")
+    print ("day(s)")
+    print (12 - month1 + month2 - 1)
+    print ("month(s)") 
+    print (year2 - year1)
+    print ("year(s)")
     
    if month1>month2 and date2>date1: 
-    print "That date is after..."
-    print  32 - date1 + 1 + date2 - 1
-    print "day(s)"
-    print 12 - month1 + month2 - 1
-    print "month(s)" 
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print  (32 - date1 + 1 + date2 - 1)
+    print ("day(s)")
+    print (12 - month1 + month2 - 1)
+    print ("month(s)") 
+    print (year2 - year1)
+    print ("year(s)")
 
 
 
 
    if month2>month1 and date1>date2: 
-    print "That date is after..."
-    print month2 - month1
-    print "month(s)"
-    print 32 - date1 + 1 + date2 - 1
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print (month2 - month1)
+    print ("month(s)")
+    print (32 - date1 + 1 + date2 - 1)
+    print (year2 - year1)
+    print ("year(s)")
     
    if month2>month1 and date1==date2: 
-    print "That date is after..."
-    print "0"
-    print "day(s)"
-    print month2 - month1
-    print "month(s)"
-    print year2 - year1
-    print "year(s)"
+    print ("That date is after...")
+    print ("0")
+    print ("day(s)")
+    print (month2 - month1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
   
    if month2>month1 and date2>date1:  
-    print "That date is after..."
-    print date2 - date1
-    print "day(s)"
-    print month2 - month1
-    print "month(s)"
-    print year2 - year1
-    print "year(s)"
-    
- 
- 
- 
-   if month2==month1 and date2==date1: 
-    print "That date is after..."
-    print date2 - date1
-    print "day(s)"
-    print month2 - month1
-    print "month(s)"
-    print year2 - year1
-    print "year(s)"
-
-print "DONE"
-    
+    print ("That date is after...")
+    print (date2 - date1)
+    print ("day(s)")
+    print (month2 - month1)
+    print ("month(s)")
+    print (year2 - year1)
+    print ("year(s)")
