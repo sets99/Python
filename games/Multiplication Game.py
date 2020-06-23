a6e00e1f41edcc0ea5ab000cd8dd7233961bb175
sakshamsethi123 <67079478+sakshamsethi123@users.noreply.github.com>
2020-06-23 18:26:57 -0400

    Delete Multiplication Game.py

--- games/Multiplication Game.py
+++
@@ -1,9 +0,0 @@
-import random
-number1=random.randint(1,100)
-number2=random.randint(1,100)
-answer=input("What is " + str(number1) + 'X' + str(number2) + '?') 
-if answer == str(number1 * number2): 
-  print("\nCorrect! Great Job!")
-  
-else: 
-  print("\nNope! The correct answer is " + str(number1 * number2) + '.')
