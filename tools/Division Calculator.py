03df31a217965b32924c4383d8cbc44fb03333c0
sakshamsethi123 <67079478+sakshamsethi123@users.noreply.github.com>
2020-06-30 16:25:56 -0400

    Update Division Calculator.py

--- tools/Division Calculator.py
+++ tools/Division Calculator.py
@@ -1,4 +1,4 @@
 a=input("What is the first term of your division problem? \n")
 b=input("What is the second term of your division problem? \n")
 quotient = float(a) / float(b)
-print quotient
+print (quotient)
