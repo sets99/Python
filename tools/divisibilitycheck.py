6131fd550d932a77b0ac5636c18a4bd5c8282e70
Saksham Sethi <67079478+sakshamsethi123@users.noreply.github.com>
2021-06-07 11:57:59 -0400

    Create divisibilitycheck.py

---
+++ tools/divisibilitycheck.py
@@ -0,0 +1,13 @@
+x = int(input("Insert the numerator: "))
+y = int(input("Insert the denominator: "))
+
+
+if x%y == 0: 
+  print(x " is divisble by ",y)
+  
+  
+else: 
+  print("No!",x, " is not divisble by ",y)
+  
+ 
+
